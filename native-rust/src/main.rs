82f027b3985c3eaea38b3bc1dea05de3f704ae88
Jesse Bryan <jesse@winneon.moe>
2017-09-14 23:20:59 -0500

    host: added more code to the rust native host

--- native-rust/src/main.rs
+++ native-rust/src/main.rs
@@ -1,26 +1,58 @@
+extern crate serde;
 extern crate serde_json;
 
+#[macro_use]
+extern crate serde_derive;
+
+use std::env;
 use std::process;
+use std::process::{Command, Stdio};
 use std::error::Error;
 use std::io::{self, BufRead};
 use std::io::prelude::*;
 use std::fs::File;
-use std::path::Path;
-use serde_json::{Value, Error as SerdeError};
+use std::path::PathBuf;
 
-fn parse_message(msg: String) {
-    /*let path = Path::new("out.txt");
-    let display = path.display();
+#[derive(Serialize, Deserialize)]
+struct Message {
+    cookies: String,
+    text: String,
+}
 
-    let mut file = match File::create(&path) {
-        Ok(file) => file,
-        Err(err) => panic!("unable to create {}: {}", display, err.description()),
+fn write_cookies(mut cookies: String) ->  PathBuf {
+    let mut path = PathBuf::new();
+
+    path.push(env::temp_dir());
+    path.push("cookies.txt");
+
+    {
+        let display = path.display();
+
+        let mut file = match File::create(&path) {
+            Ok(file) => file,
+            Err(err) => panic!("Unable to create {}: {}", display, err.description()),
+        };
+
+        cookies.push_str("\n");
+
+        match file.write_all(cookies.as_bytes()) {
+            Ok(_) => println!("Wrote the cookies file to {}", display),
+            Err(err) => panic!("Unable to write to {}: {}", display, err.description())
+        }
+    }
+
+    return path
+}
+
+fn get_code_from_line(line: String) -> i64 {
+    return if line.contains("youtube-dl failed") {
+        2
+    } else if line.contains("ERROR: Unsupported URL")
+           || line.contains("No protocol handler found to open URL") {
+        1
+    } else {
+        0
     };
-
-    match file.write_all(msg.as_bytes()) {
-        Ok(_) => println!("wrote to {}", display),
-        Err(err) => panic!("unable to write to {}: {}", display, err.description())
-    }*/
 }
 
 fn main() {
@@ -38,11 +70,34 @@ fn main() {
 
     let joined_lines = lines.join("\n");
 
-    let json: Value = match serde_json::from_str(&joined_lines) {
+    let msg: Message = match serde_json::from_str(&joined_lines) {
         Ok(json) => json,
         Err(err) => {
             eprintln!("Invalid JSON provided: {}", err);
             process::exit(1);
         },
     };
+
+    let cookies_path = write_cookies(msg.cookies.clone()).to_string_lossy().into_owned();
+
+    let mpv = match Command::new("mpv")
+        .arg("https://www.youtube.com/watch?v=piJpVXnLdJ")
+        .stdout(Stdio::piped())
+        .spawn() {
+        Ok(mpv) => mpv,
+        Err(err) => panic!("Couldn't spawn mpv: {}", err.description()),
+    };
+
+    let mut line = String::new();
+
+    match mpv.stdout.unwrap().read_to_string(&mut line) {
+        Ok(_) => {
+            let code = get_code_from_line(line.clone());
+
+            if code > 0 {
+                println!("{}", code);
+            }
+        },
+        Err(err) => panic!("Unable to read mpv stdout: {}", err.description()),
+    }
 }
